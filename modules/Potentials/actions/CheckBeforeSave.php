a14f0e3c7b50834a5552cfd0b9414e8bfd663b4f
Eric Korolev <erickorolev@gmail.com>
2021-05-21 15:03:12 +0500

    Potentials - Не даем выбрать стадию Закрыта неудачно, если поле Причина закрытия пустое

---
+++ modules/Potentials/actions/CheckBeforeSave.php
@@ -0,0 +1,37 @@
+<?php
+
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * *********************************************************************************** */
+
+class Potentials_CheckBeforeSave_Action extends Vtiger_Action_Controller {
+
+     function checkPermission(Vtiger_Request $request) {
+     	return;
+     }
+     
+     public function process(Vtiger_Request $request) {
+     	global $adb;
+     	 $dataArr = $request->get('checkBeforeSaveData');
+     	 $response = "OK";
+         $message = "";
+
+         if($request->get('detailViewAjaxMode')) {
+
+         	 if($dataArr['sales_stage'] == 'Closed Lost' && !$dataArr['cf_1252']) {
+			     $response = "ALERT";
+			     $message = "Для закрытия Сделки со статусом Закрыта неудачно вам необходимо указать причину в поле Причина закрытия";
+			 }
+			 echo json_encode(array('response' => $response, 'message' => $message));
+
+         }
+
+     	return;
+     } 
+
+ }
